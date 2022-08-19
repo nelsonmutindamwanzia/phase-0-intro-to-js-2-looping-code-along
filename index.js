cb2d7a432e8af34ab445275cee98b85564becfa5
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-19 15:21:24 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+    console.log(writeCards(['James and John', 'Mwanzia','Nelson','Mutinda','BonsaiCF'], 'graduation'));
+
+
+// Countdown Assignment
+
+function countDown(i){
+    let countdown = 10;  
+    while( countdown >= 0){
+        console.log(countdown--);
+        
+    }
+}
+console.log (countDown(10));
